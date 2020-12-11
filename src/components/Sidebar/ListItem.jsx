abee6e789db8bf61d165c60d07bbacb28b3d5bf6
Ayush Ray <ray.ayush333@gmail.com>
2020-12-12 01:19:46 +0530

    Yahan tak sab thik h

--- src/components/Sidebar/ListItem.jsx
+++ src/components/Sidebar/ListItem.jsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import {Avatar} from "@material-ui/core"
 function ListItem(){
+    const [seed,setseed] = useState(0);
+    useEffect(()=>
+    {
+        setseed(Math.floor(Math.random()*5000));
+    },[]);
     return <div className="list-item">
-        <Avatar />
+        <Avatar src={"https://avatars.dicebear.com/api/human/"+seed+".svg"} />
         <div className="chat-info">
             <h2>Room Name</h2>
             <p>Last message</p>
