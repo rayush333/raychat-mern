0748432b4ce94414747c74effe142a3219dd67ac
Ayush Ray <ray.ayush333@gmail.com>
2020-12-14 00:52:36 +0530

    Add auth with google and fb

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,14 +1,29 @@
 import React,{useState} from "react";
 import axios from "axios";
 import {Button} from "@material-ui/core";
-import {auth,provider} from "../firebase";
+import {auth,provider,fb} from "../firebase";
 import {useStateValue} from "./StateProvider";
 import { actionTypes } from "../reducer";
 function Login(){
     const [{ user },dispatch] = useStateValue();
-        function handleSubmit(event){
+        function handleSubmitGoogle(event){
         // event.preventDefault();
         auth.signInWithPopup(provider).then(async function(res){
+            console.log(res);
+            const result=await axios.post("http://localhost:5000/users",res.user);
+            console.log(result);
+            dispatch({
+                type: actionTypes.SET_USER,
+                user: result.data
+            });
+        }).catch(function(err){
+            if(err)
+            alert(err.message);
+        });
+    }
+    function handleSubmitFB(event){
+        // event.preventDefault();
+        auth.signInWithPopup(fb).then(async function(res){
             console.log(res);
             const result=await axios.post("http://localhost:5000/users",res.user);
             console.log(result);
@@ -27,8 +42,11 @@ function Login(){
             <div class="login-next">
                 <h1>Sign in to RayChat</h1>
             </div>
-            <Button type="submit" onClick={handleSubmit}>
+            <Button type="submit" onClick={handleSubmitGoogle}>
                 Sign In with Google
+            </Button>
+            <Button type="submit" onClick={handleSubmitFB}>
+                Sign In with FB
             </Button>
         </div>
     </div>;
--- src/firebase.js
+++ src/firebase.js
@@ -10,4 +10,5 @@ const firebaseConfig = {
   const firebaseApp=firebase.initializeApp(firebaseConfig);
   const auth=firebase.auth();
   const provider= new firebase.auth.GoogleAuthProvider();
-  export {auth,provider};
+  const fb = new firebase.auth.FacebookAuthProvider();
+  export {auth,provider,fb};
