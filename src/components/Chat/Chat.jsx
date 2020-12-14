615089db9fc209d3b007c07f64be0f72ed171383
Ayush Ray <ray.ayush333@gmail.com>
2020-12-15 03:38:53 +0530

    Real time functionality done

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -3,10 +3,10 @@ import ChatHeader from "./ChatHeader";
 import ChatBody from "./ChatBody";
 import ChatFooter from "./ChatFooter";
 import {useParams} from "react-router-dom";
+import Pusher from "pusher-js";
 import axios from "axios";
 function Chat(){
     const {roomid} = useParams();
-    
     const [room,updateroom] = useState({
         name: "",
         messages: []
@@ -24,6 +24,42 @@ function Chat(){
                 });
             });
         }
+        const pusher = new Pusher('39426f4afe4886d52f52', {
+            cluster: 'ap2'
+          });
+      
+          const channel = pusher.subscribe('messages');
+    //       channel.bind('inserted', function(data) {
+    //         updatechats(function(prev){
+    //           return [...prev,data];
+    //         });
+    //       });
+    //       channel.bind('deleted',function(data){
+    //         updatechats(function(prev){
+    //           return prev.filter(function(message){
+    //             return message._id !== data._id;
+    //           });
+    //         });
+    //       });
+          channel.bind('updated',function(data){
+            if(data.room_id === roomid)
+            {
+                axios.get("http://localhost:5000/rooms/"+roomid)
+                .then((res)=> {
+                    console.log(res.data);
+                    updateroom({
+                        name: res.data.name,
+                        messages: res.data.messages
+                    });
+                });
+            }
+                });
+                
+    //             return message;
+    //           });
+    //         });
+    //       });
+    //     },[]);
     },[roomid]);
     return <div className="chat">
         <ChatHeader name={room.name} id={roomid} />
