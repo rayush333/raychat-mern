c86bb2039b02aad4606e263a6465d2a7e8e080ea
Ayush Ray <ray.ayush333@gmail.com>
2020-12-17 20:45:13 +0530

    Ready for hosting

--- src/components/Chat/ChatHeader.jsx
+++ src/components/Chat/ChatHeader.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import {Avatar, IconButton} from "@material-ui/core";
 import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
 import AttachFileIcon from '@material-ui/icons/AttachFile';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
+// import MoreVertIcon from '@material-ui/icons/MoreVert';
+import AddIcon from '@material-ui/icons/Add';
 function ChatHeader(props){
     const len=props.msg.length;
     const msg=props.msg[len-1];
-
+    function addmenu(){
+        alert("Your room id is: "+props.id+"\nShare this id with your friends to add them to your private room!\nAttention: Do not share your room id with third parties.");
+    }
     return <div className="chat-header">
         <Avatar className="contact-photo" src= {"https://avatars.dicebear.com/api/human/"+props.id+".svg"} />
         <div className="chat-header-info">
@@ -16,7 +19,7 @@ function ChatHeader(props){
         <div class="chat-header-right">
         <IconButton><SearchOutlinedIcon /></IconButton>
         <IconButton><AttachFileIcon /></IconButton>
-        <IconButton><MoreVertIcon /></IconButton>
+        <IconButton><AddIcon onClick = {addmenu} /></IconButton>
         </div>
     </div>;
 }
